e4ea49126b13dc090bdce70f8f57a616f590d084
S9963hd <sanjaymajestic143@gmail.com>
2024-01-30 12:26:34 +0530

    Upgraded

--- src/Backend/GAuth.js
+++ src/Backend/GAuth.js
@@ -25,18 +25,18 @@ export default function GAuth(){
       let gAuth=new GoogleAuthProvider();
       signInWithPopup(auth,gAuth).then(result=>console.log("Authenticated")).catch(err=>console.log(err));
 }
-export function EmailAuth(email,password){
-  const firebaseConfig = {
-    apiKey: "AIzaSyBEMct0SvOwsZCMjmTUU-t_n3r98F1St3U",
-    authDomain: "frissson.firebaseapp.com",
-    projectId: "frissson",
-    storageBucket: "frissson.appspot.com",
-    messagingSenderId: "240739354609",
-    appId: "1:240739354609:web:45b56932b6c2ce41636650",
-    measurementId: "G-915B2RJRQ3"
-  };
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  let auth=getAuth();
-  signInWithEmailAndPassword(auth).then(alert("SigIn SuccessFully")).catch(err=>console.log("Error At Login"))
-}
+// export function EmailAuth(email,password){
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyBEMct0SvOwsZCMjmTUU-t_n3r98F1St3U",
+//     authDomain: "frissson.firebaseapp.com",
+//     projectId: "frissson",
+//     storageBucket: "frissson.appspot.com",
+//     messagingSenderId: "240739354609",
+//     appId: "1:240739354609:web:45b56932b6c2ce41636650",
+//     measurementId: "G-915B2RJRQ3"
+//   };
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+//   let auth=getAuth();
+//   signInWithEmailAndPassword(auth).then(alert("SigIn SuccessFully")).catch(err=>console.log("Error At Login"))
+// }
